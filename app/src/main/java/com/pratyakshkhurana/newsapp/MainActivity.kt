a71ecceae9fd380b4371af85b98009e652f56350
pratyaksh1610 <pratyakshkhuranaofficial@gmail.com>
2022-07-21 15:58:54 +0530

    fix recycler view

--- app/src/main/java/com/pratyakshkhurana/newsapp/MainActivity.kt
+++ app/src/main/java/com/pratyakshkhurana/newsapp/MainActivity.kt
@@ -2,32 +2,42 @@ package com.pratyakshkhurana.newsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.HorizontalScrollView
+import android.view.View
 import android.widget.Toast
+import androidx.appcompat.view.menu.ActionMenuItemView
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NewsItemClicked {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        recycler view needs layout manager
-        recyclerview.layoutManager = LinearLayoutManager(this)
+//        recycler view needs layout manager to ensure
+//        how data will be shown on the screen, it interacts with layout manager
+//        in this case it will be shown in form of vertical list
+        recyclerview.layoutManager = LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
+
+        val items = fetchData()
+//        passed data that is array list of string and context as listener for
+//        handling click on each view in recycler view
+//        link adapter to recycler view
+        val adapter = NewsAdapter(items, this)
 
         //link adapter to recycler view
-        val items = fetchData()
-        val adapter = NewsAdapter(items)
-        //link adapter to recycler view
-        recyclerview.adapter  = adapter
+        recyclerview.adapter = adapter
     }
-    //data passed to adapter
-    private fun fetchData() : ArrayList<String>{
+
+    //data passed to adapter which will be shown in recycler view
+    private fun fetchData(): ArrayList<String> {
         val list = ArrayList<String>()
-        for(i in 0 until 100){
-            list.add("Item ${i+1}")
+        for (i in 0 until 100) {
+            list.add("Item ${i + 1}")
         }
         return list
     }
 
+    override fun onItemClicked(item: String) {
+        Toast.makeText(this, "clicked ", Toast.LENGTH_SHORT).show()
+    }
 }
--- app/src/main/java/com/pratyakshkhurana/newsapp/NewsAdapter.kt
+++ app/src/main/java/com/pratyakshkhurana/newsapp/NewsAdapter.kt
@@ -7,31 +7,48 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.item_news.view.*
 
+//extend NewsAdapter class to recycler view adapter class and pass viewHolder
 //Adapter class , we extend and pass viewHolder in RecyclerView.Adapter<>
-//adapter needs data , which will come from activity or from API call
-class NewsAdapter(private val items : ArrayList<String>) :
+//adapter needs data , which will come from activity or from API call or from database
+//adapter needs data so it passed to it's constructor
+class NewsAdapter(private val items: ArrayList<String>, private val listener: NewsItemClicked) :
     RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
         //whenever view is created it is called
-        //inflater converts xml to view which is passed to viewHolder
-        val item = LayoutInflater.from(parent.context).inflate(R.layout.item_news,parent,false)
-        return NewsViewHolder(item)
+        //LayoutInflater class, converts xml to view which is passed to viewHolder
+        //converts R.layout.item_news (xml) to view
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_news, parent, false)
+        val viewHolder = NewsViewHolder(view)
+        view.setOnClickListener {
+            listener.onItemClicked(items[viewHolder.adapterPosition])
+        }
+        return viewHolder
     }
 
     override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
-        //binds data in holder
+        //binds data in the holder
+        //extract data from current position
+        //holder is an instance of NewsViewHolder
         val currentItem = items[position]
         holder.title.text = currentItem
     }
 
     override fun getItemCount(): Int {
-        //called once, tells total items in list
+        //called only once, returns total items in list
         return items.size
     }
+
     //we basically pass all small items that repeat in recycler view to adapter through inflating by viewHolder
     //itemView is constraint layout
-    class NewsViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
-        val title :TextView= itemView.tv
+    //pass itemView to adapter
+    class NewsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val title: TextView = itemView.tv
     }
+}
+
+//used as callback which will be handled by main activity for handling clicks of itemView
+//used for making callback
+interface NewsItemClicked {
+    fun onItemClicked(item: String)
 }
 
